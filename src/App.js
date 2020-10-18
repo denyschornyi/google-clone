44ebd7e1b222ec15a8c14ab2cadc2a0f735d2fe0
Denys Chornyi <chornijj14@gmail.com>
2020-10-18 23:36:41 +0200

    Make a short scratch with comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="app">
       <h1>I will get a job in this month</h1>
 
-      
+      {/* Home (with search) */}
+
+      <Home />
+
+      {/* Search page (The results page) */}
     </div>
   );
 }
