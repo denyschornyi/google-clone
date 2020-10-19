b6eb304ed14bf6a8a526bb2db4db6050b41c3201
Denys Chornyi <chornijj14@gmail.com>
2020-10-19 15:21:59 +0200

    Style buttons block

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,7 @@ import "./Search.css";
 
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 function Search() {
   return (
@@ -11,6 +12,11 @@ function Search() {
         <SearchIcon className="search__inputIcon" />
         <input type="text" />
         <MicIcon />
+      </div>
+
+      <div className="search__buttons">
+        <Button>Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
