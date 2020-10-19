5e8d5b72a1a7f435b942b38bb390a3b23effb675
Denys Chornyi <chornijj14@gmail.com>
2020-10-19 10:56:37 +0200

    Add Google Header nav

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import './Home.css';
+import {Link} from 'react-router-dom';
+
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core'
 
 function Home() {
     return (
         <div className="home">
-                <h1>This is Home component</h1>
-
-                <div className="home__header">
-                    <div className="home__headerLeft">
-                        {/* Link */}
-                        {/* Link */}
-                    </div>
-                    <div className="home__headerRight">
-                        {/* Link */}
-                        {/* Link */}
-                        {/* Icon */}
-                        {/* Avatar */}
-                    </div>
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
                 </div>
+                <div className="home__headerRight">
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Imgaes</Link>
+                    <AppsIcon/>
+                    <Avatar />
+                </div>
+            </div>
 
-                <div className="home__body"></div>
+            <div className="home__body"></div>
         </div>
     )
 }
