5ae6bd37906346bf88eeabb5a04becaecdd62d8b
Denys Chornyi <chornijj14@gmail.com>
2020-10-19 14:31:48 +0200

    Made a google search input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
       <Router>
-
         <Switch>
-            <Route path='/search'>
-              <h1>Search Page</h1>
-            </Route>
-            <Route path='/'>
-                <Home/>
-            </Route>
+          <Route path="/search">
+            <h1>Search Page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </Router>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import './Home.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
-import AppsIcon from '@material-ui/icons/Apps';
-import { Avatar } from '@material-ui/core'
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+
+import Search from "../components/Search";
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="home__header">
-                <div className="home__headerLeft">
-                    <Link to='/about'>About</Link>
-                    <Link to='/store'>Store</Link>
-                </div>
-                <div className="home__headerRight">
-                    <Link to='/gmail'>Gmail</Link>
-                    <Link to='/images'>Imgaes</Link>
-                    <AppsIcon/>
-                    <Avatar />
-                </div>
-            </div>
-
-            <div className="home__body">
-                <img src="https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png" alt="google logo"/>
-            </div>
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
-    )
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Imgaes</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img
+          src="https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png"
+          alt="google logo"
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
